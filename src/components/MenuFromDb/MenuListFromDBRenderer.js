1c7ab2dfd746ea8a67d5c1c08c570bab94f8589b
shyam <bkshyamprasath@gmail.com>
2018-08-18 20:48:23 +0530

    moved add item button to top

--- src/components/MenuFromDb/MenuListFromDBRenderer.js
+++ src/components/MenuFromDb/MenuListFromDBRenderer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Button } from "reactstrap";
 import { withRouter } from "react-router-dom";
 
 import MenuItemFromDB from "./MenuItemFromDB";
@@ -13,6 +13,9 @@ const styles = {
     borderRadius: "5px",
     fontWeight: "bold",
     marginTop: "100px"
+  },
+  buttonStyles: {
+    marginTop: 10
   }
 };
 
@@ -32,23 +35,24 @@ class MenusFromDBRenderer extends Component {
             Menu - {this.props.category}
           </Col>
         </Row>
+        <Row style={styles.buttonStyles}>
+          <Col sm="4" md="4" lg="4" xs="4" />
+          <Col className="text-center" sm="4" md="4" lg="4" xs="4">
+            <Button
+              outline
+              onClick={() => this.props.history.push("/Menu")}
+              color="primary"
+            >
+              {" "}
+              Add Menu Item{" "}
+            </Button>
+          </Col>
+          <Col sm="4" md="4" lg="4" xs="4" />
+        </Row>
         <Row>
           <Col sm="12" md="12" xs="12" lg="12">
             <MenuItemFromDB {...this.props} />
           </Col>
-        </Row>
-        <Row>
-          <Col sm="4" md="4" lg="4" xs="4" />
-          <Col className="text-center" sm="4" md="4" lg="4" xs="4">
-            <button
-              onClick={() => this.props.history.push("/Menu")}
-              className="btn btn-primary"
-            >
-              {" "}
-              Add Menu Item{" "}
-            </button>
-          </Col>
-          <Col sm="4" md="4" lg="4" xs="4" />
         </Row>
       </div>
     );
