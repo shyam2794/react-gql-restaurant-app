49392be41e19293a41fed9afc9b3a28a8faa337d
shyam <bkshyamprasath@gmail.com>
2018-08-18 20:50:35 +0530

    added loading , error , success in reducer

--- src/reducers/menuListFromDBReducer.js
+++ src/reducers/menuListFromDBReducer.js
@@ -1,16 +1,53 @@
-import {MENU_LIST_DB , DELETE_MENU_ITEM_DB} from '../actions';
+import {
+  MENU_LIST_DB_LOADING,
+  MENU_LIST_DB_SUCCESS,
+  MENU_LIST_DB_FAILURE,
+  DELETE_MENU_ITEM_DB
+} from "../actions";
 
-export default function (state=[], action)
-{
-  switch(action.type)
-  {
-    case MENU_LIST_DB:
-   // console.log('Menu from DB' , action.payload)
-    return action.payload
+let initialState = {
+  menuFromDB: {
+    menuList: [],
+    error: false,
+    loading: true
+  }
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case MENU_LIST_DB_LOADING:
+      return {
+        ...state,
+        menuFromDB: {
+          menuList: [],
+          loading: action.payload.loading,
+          error: false
+        }
+      };
+    case MENU_LIST_DB_SUCCESS:
+      return {
+        ...state,
+        menuFromDB: { menuList: action.payload, loading: false, error: false }
+      };
+    case MENU_LIST_DB_FAILURE:
+      return {
+        ...state,
+        menuFromDB: {
+          menuList: [],
+          loading: false,
+          error: action.payload.error_message
+        }
+      };
     case DELETE_MENU_ITEM_DB:
-   // console.log(action.payload[1]);
-    return action.payload[1];
+      return {
+        ...state,
+        menuFromDB: {
+          menuList: action.payload[1],
+          loading: false,
+          error: false
+        }
+      };
     default:
-      return state
+      return state;
   }
 }
